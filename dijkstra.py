a6e6ce1a40a932063a4c768dbc3822e1523c10ce
cepko1 <92533232+cepko1@users.noreply.github.com>
2024-02-29 20:44:54 +0200

    dijkstra's algorithm add comments

--- dijkstra.py
+++ dijkstra.py
@@ -1,9 +1,10 @@
 def find_lowest_cost_node(costs):
+    """Finding lowest cost for each nodes"""
     lowest_cost = float("inf")
     lowest_cost_node = None
     for node in costs:
         cost = costs[node]
-        if cost < lowest_cost and node not in processed:
+        if cost < lowest_cost and node not in processed:    # cheapest and not processed node
             lowest_cost = cost
             lowest_cost_node = node
     return lowest_cost_node
@@ -34,18 +35,18 @@ parents["fin"] = None
 # print(parents)
 
 processed = []   # list of checked nodes
-node = find_lowest_cost_node(costs)
-while node is not None:
+node = find_lowest_cost_node(costs)  # the frirst lowest node
+while node is not None:    # check for all nodes
     cost = costs[node]
     neighbors = graph[node]
-    for n in neighbors.keys():
+    for n in neighbors.keys():   # check all neighbors of node
         new_cost = cost + neighbors[n]
-        if costs[n] > new_cost:
+        if costs[n] > new_cost:    # check if cost lower than previous
             costs[n] = new_cost
             parents[n] = node
-    processed.append(node)
-    node = find_lowest_cost_node(costs)
+    processed.append(node)    # set node as processed
+    node = find_lowest_cost_node(costs)  # find next node
 
-for key, value in parents.items():
+for key, value in parents.items():     # show lowest path
     print(f"Node '{value}' is parent for node '{key}'")
 print(f"The minimum cost is {cost}")
